74cc3c97e2231f967d8ade01f585d97330913248
4EFI <49026937+4EFI@users.noreply.github.com>
2022-12-20 23:26:57 +0300

    Stable version

--- Examples/test.lng.tree.asm
+++ Examples/test.lng.tree.asm
@@ -44,19 +44,26 @@ pop rbx ; set "ts" pos
 
 pop [ rbx ]
 push rax
+push 3
+add
+pop rbx ; set "i" pos
+
 push 0
-add
-pop rbx ; set "a" pos
+pop [ rbx ] ; set "i"
 
-push [ rbx ] ; push "a"
-out
+
+while001:
 push rax
-push 1
+push 3
 add
-pop rbx ; set "b" pos
+pop rbx ; set "i" pos
 
-push [ rbx ] ; push "b"
-out
+push [ rbx ] ; push "i"
+push 5
+sub
+push 0
+je :endWhile001
+
 push rax
 push 2
 add
@@ -64,5 +71,39 @@ pop rbx ; set "ts" pos
 
 push [ rbx ] ; push "ts"
 out
+push rax
+push 2
+add
+pop rbx ; set "ts" pos
+
+push [ rbx ] ; push "ts"
+push 2
+pow
+push rax
+push 2
+add
+pop rbx ; set "ts" pos
+
+pop [ rbx ] ; set "ts"
+
+push rax
+push 3
+add
+pop rbx ; set "i" pos
+
+push [ rbx ] ; push "i"
+push 1
+add
+push rax
+push 3
+add
+pop rbx ; set "i" pos
+
+pop [ rbx ] ; set "i"
+
+
+jmp :while001
+endWhile001:
+
 
 hlt
